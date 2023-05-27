5ae24ea000fedcf1beef6d13ea80ed2e7eccee95
Samuel <Xylorhksshadow.sw@gmail.com>
2023-05-26 22:50:42 -0500

    Adds Enum Ecosystem Wizard and Beginning of WeaponStats

---
+++ Assets/PlayMaker Custom Scripts/My Game/EffectType.cs
@@ -0,0 +1,16 @@
+// (c) Copyright HutongGames, LLC 2010-2023. All rights reserved.
+// THIS CONTENT IS AUTOMATICALLY GENERATED.
+// [PLAYMAKER_ENUM]
+// It is recommended to use PlayMaker Enum Creator Wizard to edit this enum definition. But you can perfectly edit this script directly
+
+namespace Com.MyGame
+{
+	public enum EffectType
+	{
+		None,
+		Electric,
+		EMP,
+		Magnetic,
+		Repulsor		
+	}
+}
