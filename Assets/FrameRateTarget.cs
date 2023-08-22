2e599e26a352bedc49d51c0d5b244f3616833e81
Samuel <Xylorhksshadow.sw@gmail.com>
2023-08-21 19:12:06 -0500

    Adds FPS Limit script for testing

---
+++ Assets/FrameRateTarget.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRateTarget : MonoBehaviour
+{
+    public int FrameRate = 60;
+    void Start()
+    {
+        Application.targetFrameRate = FrameRate;
+    }
+
+   
+}
